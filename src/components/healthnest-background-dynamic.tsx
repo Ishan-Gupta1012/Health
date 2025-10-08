057357679849bea6e42b732dcd49c342a4719509
Rahul King <rahulbornking@gmail.com>
2025-10-08 19:24:17 +0000

    As a web developer, create a **modern, subtle DNA helix animation backgr

--- src/components/healthnest-background-dynamic.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import dynamic from 'next/dynamic'
-
-export const HealthNestBackgroundDynamic = dynamic(
-  () => import('./healthnest-background').then((mod) => mod.HealthNestBackground),
-  { ssr: false }
-)
