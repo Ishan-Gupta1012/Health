7aa00a8d71039672c3eefc45c906ef4b02bf46db
Rahul King <rahulbornking@gmail.com>
2025-10-08 19:33:37 +0000

    Apply this in background effect body {     margin: auto;     font-family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,11 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
+        <div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+        </div>
         {children}
         <Toaster />
       </body>
